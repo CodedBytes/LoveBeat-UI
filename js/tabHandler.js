669a18405e8c32f5592953172deb5b934a9901df
João Victor <137921917+CodedBytes@users.noreply.github.com>
2023-07-10 22:06:39 -0300

    v0.2

--- js/tabHandler.js
+++ js/tabHandler.js
@@ -1,14 +1,17 @@
 // Responsavel por controlar estilo / funcionalidade das abas
-function openTab(evt, tabNames) {
+function openTab(evt, tabNames)
+{
     var i, tabcontent, tabobj;
     tabcontent = document.getElementsByClassName("tabContent");
-    for (i = 0; i < tabcontent.length; i++) {
+    for (i = 0; i < tabcontent.length; i++)
+    {
       tabcontent[i].style.display = "none";// Seta tipo do display pra none se nao selecionado
     }
     tabobj = document.getElementsByClassName("tabObj");
-    for (i = 0; i < tabobj.length; i++) {
+    for (i = 0; i < tabobj.length; i++)
+    {
       tabobj[i].className = tabobj[i].className.replace(" active", "");// Seta active para o botao pressionado
     }
     document.getElementById(tabNames).style.display = "block";// define o tipo do display.
     evt.currentTarget.className += " active";// Nome da classe responsavel pelo active.
-  }
+}
